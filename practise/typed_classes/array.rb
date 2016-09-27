db90cfbe5aebd858186b39713842de8e69b89567
Locke <moyerock@gmail.com>
2016-09-27 15:57:37 +0800

    chore: holy cmmmits

--- practise/typed_classes/array.rb
+++ practise/typed_classes/array.rb
@@ -1,5 +1,8 @@
 lang = %w(Ruby Perl Python Scheme Pike REBOL)
 p lang
+while item = lang.pop do
+  p item
+end
 
 holy = %i(Name jack sex male age 30)
 p holy
@@ -20,4 +23,11 @@ p arr[-3..-1]
 
 arr[2..0] = %w(x y z)
 p arr
+
+arr2 = [1, [2, [3]], [4], 5]
+p arr2.flatten
+
+s = %w(i'm your father, luke)
+p s.sort_by {|w| w.length}
+p s.sort {|a, b| a <=> b }
 
