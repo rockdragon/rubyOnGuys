9322710f2f93a0bfae02f46d6a0089e544565a16
Locke <moyerock@gmail.com>
2016-09-27 13:45:48 +0800

    chore: holy cmmmits

--- practise/typed_classes/array.rb
+++ practise/typed_classes/array.rb
@@ -3,3 +3,12 @@ p lang
 
 holy = %i(Name jack sex male age 30)
 p holy
+
+alpha = %w(a b c d e)
+p alpha.push('f')
+p alpha.shift
+p alpha
+p alpha.unshift('g')
+p alpha.pop
+p alpha
+p alpha.concat(%w(h i j k l))
