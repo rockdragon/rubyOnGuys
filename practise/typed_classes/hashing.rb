035f5485fcff56ddd362d395ece816dafc5f6586
Locke <moyerock@gmail.com>
2016-09-29 01:09:32 +0800

    chore:holy

--- practise/typed_classes/hashing.rb
+++ practise/typed_classes/hashing.rb
@@ -1,12 +1,15 @@
 person = {'name' => 'tom', 'sex' => 'male', 'age' => 18}
 p person
-
+p person.class
 
 person2 = {name: 'tom', sex: 'male', age: 18}
 p person2
+p person2.class
 
 person3 = Hash.new
 person3['name'] = 'tom'
 person3['sex'] = 'male'
 person3['age'] = 18
 p person3
+p person3.class
+p person3.class.ancestors
