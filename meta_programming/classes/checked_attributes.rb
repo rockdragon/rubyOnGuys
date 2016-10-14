f94ecf5077f6278441e61a80e4e49132c680f1d9
Locke <moyerock@gmail.com>
2016-10-14 09:41:21 +0800

    feat: attr_checked

--- meta_programming/classes/checked_attributes.rb
+++ meta_programming/classes/checked_attributes.rb
@@ -1,15 +1,27 @@
 module CheckedAttributes
-  def add_checked_attribute(klass, attribute)
-    eval <<-end_block
-      class #{klass}
-        def #{attribute}=(value)
-          raise 'Invalid attribute' unless value
-          @#{attribute} = value
-        end
-        def #{attribute}()
-          @#{attribute}
-        end
+  def add_checked_attribute(klass, attribute, &validation)
+    klass.class_eval do
+      define_method "#{attribute}=" do |value|
+        raise 'Invalid attribute' unless validation.call(value)
+        instance_variable_set("@#{attribute}", value)
       end
-    end_block
+
+      define_method attribute do
+        instance_variable_get "@#{attribute}"
+      end
+    end
+  end
+
+  class Class
+    def attr_checked(attribute, &validation)
+      define_method "#{attribute}=" do |value|
+        raise 'Invalid attribute' unless validation.call(value)
+        instance_variable_set "@#{attribute}"
+      end
+
+      define_method attribute do
+        instance_variable_get "@#{attribute}"
+      end
+    end
   end
 end
