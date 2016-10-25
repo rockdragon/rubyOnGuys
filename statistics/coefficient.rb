7273e575d74ada13025f284aa92955d694c11eab
Locke <moyerock@gmail.com>
2016-10-26 00:02:07 +0800

    fix: some calc errors

--- statistics/coefficient.rb
+++ statistics/coefficient.rb
@@ -1,5 +1,14 @@
 require_relative 'stat_lib'
 
-array = [22, 23.6, 22, 170.8, 2016.35, 466]
+array = [10.0, 8.5, 10.0, 10.2, 10.6, 10.5, 9.8, 9.7, 9.5, 9.3]
 col = StatLib::Collection.new(array)
-p col.sk.to_f
+
+p array
+p %Q{和:             #{col.sum(array).round(2)} }
+p %Q{均:             #{col.average.to_f } }
+p %Q{平均(绝对离)差:  #{col.mean_deviation.to_f } }
+p %Q{方差:           #{col.variation.to_f } }
+p %Q{标准差:         #{col.standard_deviation.to_f } }
+p %Q{离散系数:       #{col.discreteness.to_f } }
+p %Q{偏态系数:       #{col.skewness.to_f } }
+p %Q{峰态系数:       #{col.kurtosis.to_f } }
--- statistics/stat_lib.rb
+++ statistics/stat_lib.rb
@@ -5,41 +5,71 @@ module StatLib
     def initialize(array)
       @array = array.collect { |x| BigDecimal.new(x.to_s) }
     end
+
     #和
     def sum(array)
-      array.reduce(&:+)
+      ; array.reduce(&:+);
     end
+
     #平均数
-    def average
-      self.sum(@array) / (@array.size + 0.0)
+    def average;
+      self.sum(@array) / (@array.size + 0.0);
     end
+
     #平均(绝对离)差
-    def mean_deviation
-      sum(differences) / (@array.size + 0.0)
+    def mean_deviation;
+      sum(differences_abs) / (@array.size + 0.0);
     end
+
     #方差
-    def variance
-      sum(differences) ** 2 / (@array.size + 0.0)
+    def variation;
+      sum(differences_power_n(2)) / (@array.size + 0.0);
     end
+
     #标准差
-    def standard_deviation
-      Math.sqrt(self.variance)
+    def standard_deviation;
+      Math.sqrt(self.variation);
+    end
+
+    #离散系数
+    def discreteness;
+      standard_deviation / average;
     end
 
     #偏态系数
-    def sk
+    def skewness
       n = @array.size
-      molecule = sum(differences) ** 3
+      molecule = sum(differences_power_n(3))
       denominator = (n - 1) * (n - 2) * standard_deviation ** 3
       molecule / (denominator + 0.0)
     end
 
+    #峰态系数
+    def kurtosis
+      n = @array.size
+      molecule = n * (n + 1) * sum(differences_power_n(4)) - 3 * sum(differences_power_n(2)) ** 2 * (n - 1)
+      denominator = (n - 1) * (n - 2) * (n - 3) * standard_deviation ** 4
+      molecule / (denominator + 0.0)
+    end
+
     private
-      #平均差集合
-      def differences
-        x_ = self.average
-        @array.collect { |x| x - x_ }
-      end
+    #平均差集合
+    def differences
+      x_ = self.average
+      @array.collect { |x| x - x_ }
+    end
+
+    #平均差绝对值集合
+    def differences_abs
+      x_ = self.average
+      @array.collect { |x| (x - x_).abs }
+    end
+
+    #平均差平方
+    def differences_power_n(n)
+      x_ = self.average
+      @array.collect { |x| (x - x_) ** n }
+    end
   end
 
 end
