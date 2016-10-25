193854af8b65fa06ab6e6927ae7d8a05b8d7c4be
Locke <moyerock@gmail.com>
2016-10-26 00:05:00 +0800

    fix: some calc errors

--- statistics/coefficient.rb
+++ statistics/coefficient.rb
@@ -8,7 +8,7 @@ p %Q{和:             #{col.sum(array).round(2)} }
 p %Q{均:             #{col.average.to_f } }
 p %Q{平均(绝对离)差:  #{col.mean_deviation.to_f } }
 p %Q{方差:           #{col.variation.to_f } }
-p %Q{标准差:         #{col.standard_deviation.to_f } }
-p %Q{离散系数:       #{col.discreteness.to_f } }
-p %Q{偏态系数:       #{col.skewness.to_f } }
-p %Q{峰态系数:       #{col.kurtosis.to_f } }
+p %Q{标准差:          #{col.standard_deviation.to_f } }
+p %Q{离散系数:        #{col.discreteness.to_f } }
+p %Q{偏态系数:        #{col.skewness.to_f } }
+p %Q{峰态系数:        #{col.kurtosis.to_f } }
