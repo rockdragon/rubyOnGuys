fa4e340b4bbd4b8649e2b08857892e0048caa83b
Locke <moyerock@gmail.com>
2016-11-09 00:10:35 +0800

    chore: practices

--- statistics/haed_first/practice_264.rb
+++ statistics/haed_first/practice_264.rb
@@ -6,6 +6,6 @@ p '选5人上场'
 p %Q{C(#{n}, #{r}) :            #{Lib.combination(n, r)} }
 String.puts_hr
 
-p '随机选三名主力，即从9人中选2人'
+p '随机选三名主力'
 p %Q{C(#{n}, 3) :            #{Lib.combination(9, 2)} }
-p %Q{同时在场的概率 :        #{Lib.combination(9, 2).fdiv(Lib.combination(n, r)).round(4) } }
+p %Q{3人同时在场,即从9人中选2人 :        #{Lib.combination(9, 2).fdiv(Lib.combination(n, r)).round(4) } }
---
+++ statistics/haed_first/practice_265.rb
@@ -0,0 +1,15 @@
+require_relative '../libs/exts/lib'
+
+n = 52
+r = 5
+p '选一手,5张:'
+p %Q{C(#{n}, #{r}) :            #{Lib.combination(n, r)} }
+String.puts_hr
+
+p %Q{拿10-K同花顺概率 :        #{4.fdiv(Lib.combination(n, r))} }
+String.puts_hr
+
+p %Q{拿到炸的概率 :            #{(13 * Lib.combination(n - 4, 1)).fdiv(Lib.combination(n, r))} }
+String.puts_hr
+
+p %Q{五张同花概率 :            #{(4 * Lib.combination(13, 5)).fdiv(Lib.combination(n, r)) } }
