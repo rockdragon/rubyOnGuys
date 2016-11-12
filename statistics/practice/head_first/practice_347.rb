6636259400eb4e41a34a8254ddadba88159009a2
Locke <moyerock@gmail.com>
2016-11-13 00:01:51 +0800

    chore: standard score

--- statistics/practice/head_first/practice_347.rb
+++ statistics/practice/head_first/practice_347.rb
@@ -1,4 +1,9 @@
 u = 10
 o = 2
 x = 6
-puts %Q{N(10, 4), x=6, 则 z = #{(x - u).fdiv(o)}}
+puts %Q{N(#{u}, #{o ** 2}), x=#{x}, 则 z = #{(x - u).fdiv(o)}}
+
+u = 6.3
+o = 3
+x = 0.3
+puts %Q{N(#{u}, #{o ** 2}), x=#{x}, 则 z = #{(x - u).fdiv(o)}}
