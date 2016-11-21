403ecb257c795ca97fdb92069b6b30786c9dace7
Locke <moyerock@gmail.com>
2016-11-21 23:17:11 +0800

    chore: renaming

--- statistics/libs/distributions/t_distribution.rb
+++ statistics/libs/distributions/t_distribution.rb
@@ -4,7 +4,7 @@ module StatLib
   # T分布
   #   适用于样本n很小,不知道总体方差的确切值
   class TDistribution
-    # t分布
+    # t分布的标准分
     def self.t_xsnu(x, s2, n, u)
       (x - u).fdiv(Math.sqrt(s2.fdiv(n)))
     end
