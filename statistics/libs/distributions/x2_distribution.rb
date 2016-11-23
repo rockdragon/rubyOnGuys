e03a3d99fcafa531da5977eab431d0d1949be19d
Locke <moyerock@gmail.com>
2016-11-23 18:32:10 +0800

    chore: add expectation of x2

--- statistics/libs/distributions/x2_distribution.rb
+++ statistics/libs/distributions/x2_distribution.rb
@@ -5,13 +5,23 @@ module StatLib
   class X2Distribution
     # 求卡方分布的　检验统计量
     #   array中哈希字段 {:o, :e}
-    #     o 观测　e　期望
+    #     o 观测　
+    #     e　期望
     def self.x2(array)
       array.inject(0) { |sum, ele| sum + ((ele[:o] - ele[:e]) ** 2).fdiv(ele[:e]) }
     end
 
+    # 期望频数
+    #   sum_h 行合计
+    #   sum_k 列合计
+    #   sum 总和
+    def self.ex(sum_h, sum_k, sum)
+      (sum_h * sum_k).fdiv(sum)
+    end
+
     # 自由度
-    #     h 行   k 列
+    #   h 行
+    #   k 列
     def self.degree_of_freedom(h, k)
       (h - 1) * (k - 1)
     end
