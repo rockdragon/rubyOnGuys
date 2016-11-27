341c944f5f795b9b12aa1ab1f8f4b1be1ae13fa2
Locke <moyerock@gmail.com>
2016-11-27 21:46:35 +0800

    feat: F distribution

---
+++ statistics/libs/distributions/f_distribution.rb
@@ -0,0 +1,18 @@
+module StatLib
+  require_relative '../exts/lib'
+
+  # F分布
+  class FDistribution
+    # 期望
+    def self.ex(n)
+      n.fdiv(n - 2)
+    end
+
+    # 方差
+    def self.variance(m, n)
+      molecule = 2 * (n ** 2) * (m + n - 2)
+      denominator =  m * (n - 2) * (n - 4)
+      molecule.fdiv(denominator)
+    end
+  end
+end
